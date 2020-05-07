7cdf9f0f089e8096fc2b89064799744e142d49f8
guemache <guemacheriadh09@gmail.com>
2020-05-07 04:10:50 +0000

    Update antecedents_pronoms.py

--- resolution_coreferences_pronominales/antecedents_pronoms.py
+++ resolution_coreferences_pronominales/antecedents_pronoms.py
@@ -2,30 +2,36 @@ import spacy
 
 nlp = spacy.load('fr')
 
-
 def antecedents_et_verbe_des_pronoms(phrase):
     phrase = nlp(phrase)
     noms = []
     pronoms = []
-    i = 0
-    for jeton in phrase:
-        if jeton.pos_ == 'PRON':
-            i += 1
-    j = 0
-    for jeton in phrase:
-        if jeton.pos_ == 'NOUN' or jeton.pos_ == 'PROPN':
-            if 'nsubj' in jeton.dep_:
-                noms.append(['->', jeton, jeton.head])
-            if jeton.dep_ == 'obj':
-                noms.append(['<-', jeton, jeton.head])
-            if jeton.dep_ == 'amod':
-                noms.append([jeton, jeton.head])
-        if jeton.pos_ == 'PRON':
-            j += 1
-            if jeton.dep_ == 'nsubj':
-                pronoms.extend([jeton, [jeton.head], noms])  # relations sortantes du pronom
-            if jeton.dep_ == 'iobj':
-                pronoms.extend([[jeton.head], jeton, noms])  # relations entrantes vers le pronom
-            if i == j:
-                break
-    return pronoms
+    antecedents= [] 
+    nomsAnt=[]
+    relationsPronom=[]
+    i=0
+    for jeton in doc:
+        i+=1
+        if jeton.pos_=='PRON':
+            j=0
+            for jetonAux in doc:
+                j+=1
+                if jetonAux.pos_ == 'NOUN' or jetonAux.pos_=='PROPN':
+                    nomsAnt.extend([jetonAux])
+                    if('nsubj' in jetonAux.dep_):
+                        noms.append(['->',jetonAux,jeton.head])
+                    if(jetonAux.dep_=='obj'):
+                        noms.append(['<-',jetonAux,jeton.head])
+                    if(jetonAux.dep_=='amod'):
+                        noms.append([jetonAux,jetonAux.head])
+                if(i==j):
+                    antecedents.extend([jeton,nomsAnt])
+                    nomsAnt=[]
+                    if(jeton.dep_=='nsubj'):
+                        relationsPronom.extend([jeton,['->',jeton.head]])
+                        pronoms.extend([jeton,[jeton.head],noms]) #relations sortantantes du pronom
+                    if(jeton.dep_=='iobj'):
+                        relationsPronom.extend([jeton,['<-',jeton.head]])
+                        pronoms.extend([[jeton.head],jeton,noms]) #relations entrantes vesrs le pronom
+                    break
+    return antecedents
