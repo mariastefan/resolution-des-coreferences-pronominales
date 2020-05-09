b58286e0a274274d15bdecc4b113d9f3fcea97c4
Maria Stefan <maria.stefan@live.fr>
2020-05-10 00:18:38 +0200

    Modifications extractions_mot.py (voir description pour détails)
    Correction bug mots composés dans relations_mot()
    Ajout fonction mot_sans_espaces()
    Correction bug de relations_entre_mots() lorsqu'un des mots est inexistant dans jdm.
    Modifs fichier de test pour relations_entre_mots()
    Ajout fichier de test pour les les mots composés

---
+++ resolution_coreferences_pronominales/tests/test-extraction_mot-mots-composes.py
@@ -0,0 +1,12 @@
+import extraction_mot
+import os
+
+if __name__ == '__main__':
+    mot = 'être vivant'
+    extraction_mot.relations_mot(mot, 'all', True)
+    chemin = os.path.dirname(os.path.abspath(__file__)).split('/tests')[0]
+    chemin_fichier = os.path.join(chemin, 'cache', extraction_mot.mot_sans_espaces(mot) + '_' + 'all' + '.pkl')
+    if os.path.isfile(chemin_fichier):
+        print('Le fichier\n' + chemin_fichier + '\na bien été créé (ou existait déjà)')
+    else:
+        print('Erreur création fichier ' + chemin_fichier)
