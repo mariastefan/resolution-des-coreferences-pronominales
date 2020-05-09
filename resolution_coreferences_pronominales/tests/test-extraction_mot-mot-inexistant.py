da0981d120adbb707b96082efde1eeb01f29d51e
Maria Stefan <maria.stefan@live.fr>
2020-05-09 19:22:21 +0200

    Suppression ligne inutile et création fichier data/relations_manquantes

--- resolution_coreferences_pronominales/tests/test-extraction_mot-mot-inexistant.py
+++ resolution_coreferences_pronominales/tests/test-extraction_mot-mot-inexistant.py
@@ -3,10 +3,8 @@ import extraction_mot
 if __name__ == '__main__':
     # Paramètres à choisir :
     mot = 'adsqmour'
-
     relations_mot = extraction_mot.relations_mot(mot, 'all', False)
     if relations_mot is None:
         print("Le mot " + mot + " n'existe pas sur jeuxdemots.org !")
     else:
         print("Il y a " + str(len(relations_mot)) + " entrées dans relations_mot")
-    extraction_mot.supprimer_cache()
